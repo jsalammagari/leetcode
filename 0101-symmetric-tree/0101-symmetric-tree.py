fb1042e787312a6fc352718b912d9f69dfc8ed42
Jahnavi Salammagari <143347797+jsalammagari@users.noreply.github.com>
2025-09-20 15:25:54 -0700

    Time: 0 ms (100%), Space: 17.7 MB (97.35%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,14 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.isMirror(root.left,root.right)
-    def isMirror(self,node1,node2):
-        if not node1 and not node2:
+        # If tree is empty, it is symmetric
+        if not root:
             return True
-        if not node1 or not node2:
-            return False
-        if node1.val != node2.val:
-            return False
-        return self.isMirror(node1.left,node2.right) and self.isMirror(node1.right,node2.left)
+
+        # Use a queue to store pairs of nodes for comparison
+        queue = [root.left, root.right]
+
+        # Process until queue is empty
+        while queue:
+            # Pop two nodes at a time (they should be mirror images)
+            t1 = queue.pop(0)
+            t2 = queue.pop(0)
+
+            # If both are None, continue to next pair
+            if not t1 and not t2:
+                continue
+
+            # If only one is None OR values don't match → not symmetric
+            if not t1 or not t2 or t1.val != t2.val:
+                return False
+
+            # Add children in mirrored order:
+            # Left child of t1 with Right child of t2
+            queue.append(t1.left)
+            queue.append(t2.right)
+
+            # Right child of t1 with Left child of t2
+            queue.append(t1.right)
+            queue.append(t2.left)
+
+        # If all pairs matched → symmetric
+        return True
